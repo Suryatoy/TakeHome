5c1bb9bc19572e7d2f55726de60cd6acb5adaa79
Suryatoy <Surya.friendz@gmail.com>
2020-06-21 11:00:01 -0400

    Adding parcelize annotation

--- app/src/main/java/com/surya/githubuserdetails/model/repository/RepositoriesItem.kt
+++ app/src/main/java/com/surya/githubuserdetails/model/repository/RepositoriesItem.kt
@@ -1,45 +1,14 @@
 package com.surya.githubuserdetails.model.repository
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class RepositoriesItem(
     val description: String?,
     val forks: String?,
     val name: String?,
     val stargazers_count: String?,
     val updated_at: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
+) : Parcelable
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(description)
-        parcel.writeString(forks)
-        parcel.writeString(name)
-        parcel.writeString(stargazers_count)
-        parcel.writeString(updated_at)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<RepositoriesItem> {
-        override fun createFromParcel(parcel: Parcel): RepositoriesItem {
-            return RepositoriesItem(
-                parcel
-            )
-        }
-
-        override fun newArray(size: Int): Array<RepositoriesItem?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
