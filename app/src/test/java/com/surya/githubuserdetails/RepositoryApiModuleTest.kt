0fbc4b0cd519f211c80fc476281f3ebe77082372
Suryatoy <Surya.friendz@gmail.com>
2020-06-21 12:25:09 -0400

    Test Code cleanup

--- app/src/test/java/com/surya/githubuserdetails/RepositoryApiModuleTest.kt
+++ app/src/test/java/com/surya/githubuserdetails/RepositoryApiModuleTest.kt
@@ -3,7 +3,7 @@ package com.surya.githubuserdetails
 import com.surya.githubuserdetails.di.repository.RepositoryApiModule
 import com.surya.githubuserdetails.model.repository.RepositoryApiService
 
-class RepositoryApiModuleTest(val mockRepositoryApiService: RepositoryApiService) :
+class RepositoryApiModuleTest(private val mockRepositoryApiService: RepositoryApiService) :
     RepositoryApiModule() {
 
     override fun getRepositoryApiService(): RepositoryApiService {
--- app/src/test/java/com/surya/githubuserdetails/UserApiModuleTest.kt
+++ app/src/test/java/com/surya/githubuserdetails/UserApiModuleTest.kt
@@ -3,7 +3,7 @@ package com.surya.githubuserdetails
 import com.surya.githubuserdetails.di.user.UserApiModule
 import com.surya.githubuserdetails.model.user.UserApiService
 
-class UserApiModuleTest(val mockUserApiService:UserApiService): UserApiModule() {
+class UserApiModuleTest(private val mockUserApiService:UserApiService): UserApiModule() {
 
     override fun getUserApiService(): UserApiService {
         return mockUserApiService
